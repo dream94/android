6d4b23aad343b9f7fe445f9ca414025d9c449a19
dream94 <2409372469@qq.com>
2015-06-01 22:32:01 +0800

    GridView游戏

--- GridView/app/src/main/java/com/example/admin/game1/GameStart.java
+++ GridView/app/src/main/java/com/example/admin/game1/GameStart.java
@@ -80,7 +80,7 @@ public class GameStart extends Activity {
         }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                dialog.cancel();
             }
         }).create().show();
     }
